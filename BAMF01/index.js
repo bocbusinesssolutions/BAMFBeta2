c99a6ba9500b27fbc8deb02686555ab0c790fbcc
BOC Business Solutions <35924867+bocbusinesssolutions@users.noreply.github.com>
2018-03-25 11:43:35 -0700

    Update index.js

--- BAMF01/index.js
+++ BAMF01/index.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var fs = require('fs');
+var dt = require('./module1'); //Call module
 http.createServer(function (req, res) {
   //Open a file on the server and return it's content:
   fs.readFile('web/index.html', function(err, data) {
