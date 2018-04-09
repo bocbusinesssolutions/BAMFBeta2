2207fe2fe6080da8ae3bed6b5b7cee5a7ada7b31
BOC Business Solutions <35924867+bocbusinesssolutions@users.noreply.github.com>
2018-04-08 21:59:11 -0400

    Update buildBAMF.sh

--- buildBAMF.sh
+++ buildBAMF.sh
@@ -5,8 +5,6 @@
 #
 systemctl start docker
 systemctl enable docker
-mkdir /usr/local/nodejs
-cd /usr/local/nodejs
 yum install iputils git nodejs wget tar unzip make
 iptables -A INPUT -p tcp --dport 3000 -j ACCEPT
 iptables -A OUTPUT  -p tcp --dport 3000 -j ACCEPT
@@ -16,5 +14,3 @@ npm i socket.io
 npm i url
 npm i sys
 npm i path
-git clone https://github.com/bocbusinesssolutions/BAMFBeta2.git
-
