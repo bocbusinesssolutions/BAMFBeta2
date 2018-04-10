5ea3ac65d160df2864cee43c42ccc86a3c5b517a
BOC Business Solutions <35924867+bocbusinesssolutions@users.noreply.github.com>
2018-04-09 22:24:07 -0400

    Update buildBAMF.sh

--- buildBAMF.sh
+++ buildBAMF.sh
@@ -1,8 +1,18 @@
 #Prerequisites:
-#  1. Install PhotonOS
-#  2. Change root password
-#  3. Run vi /etc/resolv.conf - add 8.8.8.8 and 8.8.8.4 as nameservers
+#  1a. Deploy PhotonOS
+#  2a. Change root password
+#  3a. Run vi /etc/resolv.conf - add 8.8.8.8 and 8.8.8.4 as nameservers
+#  4a. Run yum install git
+#  5a. Clone BAMFBeta2 repository
 #
+#  1b. Deploy RHEL
+#  2b. Login using SSH security key
+#  3b. Run curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
+#  4b. Run curl --silent --location https://rpm.nodesource.com/setup_9.x | sudo bash -
+#  5b. Run sudo yum -y install nodejs
+#  6b. Run sudo yum install git
+#  7b. Clone BAMFBeta2 repository
+#  
 systemctl start docker
 systemctl enable docker
 yum install iputils git nodejs wget tar unzip make
